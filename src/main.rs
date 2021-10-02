66d6fddddff92e98d5bf5617981098e8b4bba21f
phillinux94 <phillinux94@gmail.com>
2021-10-02 22:48:09 +0200

    Jeu du plus ou du moins

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,36 @@
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
-    println!("Devinez le nombre");
+    println!("Devinez le nombre !");
 
-    println!("Veuillez entrer un nombre.");
+    let nombre_secret = rand::thread_rng().gen_range(1..101);
 
-    let mut supposition = String::new();
+    // -- partie masquée ici --
 
-    io::stdin()
-        .read_line(&mut supposition)
-        .expect("Échec de la lecture de l'entrée utilisateur");
+    println!("Le nombre secret est : {}", nombre_secret);
 
-    println!("Votre nombre : {}", supposition);
+    loop {
+        println!("Veuillez entrer un nombre.");
+
+        // -- partie masquée ici --
+
+
+        let mut supposition = String::new();
+
+        io::stdin()
+            .read_line(&mut supposition)
+            .expect("Échec de la lecture de l'entrée utilisateur");
+
+        let supposition: u32 = supposition.trim().parse().expect("Veuillez entrer un nombre !");
+
+        println!("Votre nombre : {}", supposition);
+
+        match supposition.cmp(&nombre_secret) {
+            Ordering::Less => println!("C'est plus !"),
+            Ordering::Greater => println!("C'est moins !"),
+            Ordering::Equal => println!("Vous avez gagné !"),
+        }
+    }
 }
